c17d6693f911d66540e4923ba16053a2222c985d
Alexander Katkov <afargoth@gmail.com>
2022-07-09 11:41:04 +0300

    Added Sandbox Task A

---
+++ Sandbox/Task-A/task-A.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+public class Program
+{
+    static TextReader reader;
+    static TextWriter writer;
+
+    public static void Main()
+    {
+        reader = Console.In;
+        writer = Console.Out;
+        
+        int n = int.Parse(reader.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            SolveTheTask();
+        }
+    }
+
+    private static void SolveTheTask()
+    {
+        string input = reader.ReadLine();
+        int result = input.Split(' ').Select(x => int.Parse(x)).Sum();
+        writer.WriteLine(result);
+    }
+}
