8c4c7e63ced04bbac82b62c60881419363c4d27c
Alexander Katkov <afargoth@gmail.com>
2022-07-10 16:04:42 +0300

    Added Contest Task E

---
+++ Contest/Task-E/task-E.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Program
+{
+    static TextReader reader;
+    static TextWriter writer;
+
+    public static void Main()
+    {
+        reader = Console.In;
+        writer = Console.Out;
+        
+        int n = int.Parse(reader.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            SolveTheTask();
+        }
+    }
+
+    private static void SolveTheTask()
+    {
+        _ = reader.ReadLine();
+        string input = reader.ReadLine();
+        string[] values = input.Split(' ');
+
+        HashSet<string> tasks = new();
+        string prev = String.Empty;
+
+        foreach (string value in values)
+        {
+            if (!String.IsNullOrEmpty(prev) && (value == prev))
+                continue;
+
+            if (tasks.Contains(value))
+            {
+                writer.WriteLine("NO");
+                return;
+            }
+            
+            tasks.Add(value);
+            prev = value;
+        }
+
+        writer.WriteLine("YES");
+    }
+}
