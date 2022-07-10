03dfde50b29ef26470efd9c1e6f8c7ee1bd0dcb9
Alexander Katkov <afargoth@gmail.com>
2022-07-10 14:55:09 +0300

    Added Contest Task A

---
+++ Contest/Task-A/task-A.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+public class Program
+{
+    static TextWriter writer;
+
+    public static void Main()
+    {
+        writer = Console.Out;
+        
+        SolveTheTask();
+    }
+
+    private static void SolveTheTask()
+    {
+        writer.WriteLine("I am sure that I will fill out the form by 11:00 am on July 4, 2022.");
+    }
+}
