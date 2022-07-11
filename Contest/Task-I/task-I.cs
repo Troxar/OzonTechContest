d975759d3268b1571b98f55ee69b066811063b6e
Alexander Katkov <afargoth@gmail.com>
2022-07-11 18:34:11 +0300

    Added Contest Task I

---
+++ Contest/Task-I/task-I.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Program
+{
+    static TextReader reader;
+    static TextWriter writer;
+
+    public static void Main()
+    {
+        reader = Console.In;
+        writer = Console.Out;
+        
+        SolveTheTask();
+    }
+
+    private static void SolveTheTask()
+    {
+        string input = reader.ReadLine();
+        string[] values = input.Split(' ');
+
+        int procCount = int.Parse(values[0]);
+        int taskCount = int.Parse(values[1]);
+
+        Proc[] procs = new Proc[procCount];
+
+        input = reader.ReadLine();
+        values = input.Split(' ');
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            procs[i] = new Proc { Number = i + 1, Energy = int.Parse(values[i]) };
+        }
+
+        var free = new PriorityQueue<Proc, int>(procs.Select(x => (x, x.Energy)));
+        var used = new PriorityQueue<Proc, int>();
+
+        ulong result = 0;
+        for (int i = 0; i < taskCount; i++)
+        {
+            input = reader.ReadLine();
+            values = input.Split(' ');
+
+            int taskTick = int.Parse(values[0]);
+            int taskWork = int.Parse(values[1]);
+
+            while ((used.Count != 0) && (used.Peek().Free <= taskTick))
+            {
+                Proc freed = used.Dequeue();
+                free.Enqueue(freed, freed.Energy);
+            }
+
+            if (free.Count == 0)
+                continue;
+
+            Proc proc = free.Dequeue();
+            proc.Free = taskTick + taskWork;
+            used.Enqueue(proc, proc.Free);
+
+            result += (ulong)taskWork * (ulong)proc.Energy;
+        }
+
+        writer.WriteLine(result);
+    }
+
+    class Proc
+    {
+        public int Number { get; set; }
+        public int Energy { get; set; }
+        public int Free { get; set; }
+    }
+}
