52e1c0cd2d242af52a4e33edc72b0b10bde8f182
Alexander Katkov <afargoth@gmail.com>
2023-02-15 11:11:24 +0700

    Add 2023-02 Task S1

---
+++ 2023-02/Task-S1/task-S1.sql
@@ -0,0 +1,10 @@
+select 
+  problems.id, 
+  problems.contest_id, 
+  problems.code
+from problems
+join submissions on submissions.problem_id = problems.id
+where submissions.success
+group by problems.id
+having count(distinct submissions.user_id) >= 2
+order by problems.id
